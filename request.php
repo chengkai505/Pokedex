7c183fd078c7eb29a751c5220a5f18e973fe5466
Kai Wu <wuvincentck505@gmail.com>
2020-10-24 15:13:04 +0800

    Modify request.php
    Make request.php be able to run in cli and web.

--- request.php
+++ request.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-if (!isset($_GET['id'])) exit();
 class Pokedex {
 	public $ID;
 	public $Type = array();
@@ -8,14 +6,22 @@ class Pokedex {
 	public $English;
 	public $Japanese;
 }
+
+if (!isset($argv[1])) {
+	header('Content-Type: application/json; charset=utf-8');
+}
+
+$request = intval(isset($argv[1]) ? $argv[1] : $_GET['q'], 10);
+
+if (!$request) exit();
+
 $document = new DomDocument;
 $document->load('https://wiki.52poke.com/zh-hant/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E5%88%97%E8%A1%A8%EF%BC%88%E6%8C%89%E5%85%A8%E5%9B%BD%E5%9B%BE%E9%89%B4%E7%BC%96%E5%8F%B7%EF%BC%89/%E7%AE%80%E5%8D%95%E7%89%88');
 $data = $document->getElementsByTagName('table')[2]->getElementsByTagName('tr');
-$i = 0;
 foreach ($data as $row) {
 	$td = $row->getElementsByTagName('td');
-	if (sizeof($td) != 4) continue;
-	if (intval(substr($td[0]->nodeValue, 1), 10) == $_GET['id']) {
+	if ($td->length != 4) continue;
+	if (intval(substr($td[0]->nodeValue, 1), 10) == $request) {
 		$singleData = new Pokedex;
 		$singleData->ID = intval(substr($td[0]->nodeValue, 1), 10);
 		$singleData->Chinese = substr($td[1]->nodeValue, 0, -1);
--- updator.php
+++ updator.php
@@ -1,7 +1,7 @@
 <?php
 $origin = json_decode(file_get_contents('index.json'), true);
 for ($i = 0; $i < 893; $i++) {
-	$origin[$i] = json_decode(file_get_contents('https://path.to/request.php?id=' . ($i + 1)), true);
+	$origin[$i] = json_decode(shell_exec('php request.php ' . ($i + 1)), true);
 	file_put_contents('index.json', json_encode($origin, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 	printf("\r%03d", $i + 1);
 }
